d0cf4ed0170fce57dccff196d97a09b4f9d8c395
WilhelmCZ <wilhelm.smaia@senacsp.edu.br>
2024-03-21 17:17:45 -0300

    cadastra usuario SQL pelo VISUAL STUDIO LOKO Aula dia 21/03
    LESGO

--- ChamadosTecnicosTec55/Adicionar/frmAdicionarCliente.cs
+++ ChamadosTecnicosTec55/Adicionar/frmAdicionarCliente.cs
@@ -37,7 +37,34 @@ namespace ChamadosTecnicosTec55.Adicionar
 
             if(string.IsNullOrWhiteSpace(txbNome.Text) || string.IsNullOrWhiteSpace(txbObs.Text) || string.IsNullOrWhiteSpace(txbProfissao.Text) || string.IsNullOrWhiteSpace(txbSetor.Text))
             {
-                MessageBox.Show("CADE OS DADOSSS ??");
+                MessageBox.Show("donde esta el dados?!");
+            }
+            else
+            {
+                        //Toda vez que mexer com bd usar try
+                        try
+                        {
+                        //preencha o objeto cliente
+                        cliente.Nome = txbNome.Text;
+                        cliente.Profissao = txbProfissao.Text;
+                        cliente.Setor = txbSetor.Text;
+                        cliente.Obs = txbObs.Text;
+
+
+
+                        //CHAMA o dao para incluir o cliente
+                        clientedao.IncluiCliente(cliente);
+
+                } catch (Exception ex)
+                    {
+                    MessageBox.Show("Error Error power is down wubbbox","nao",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    }
+                finally
+                {
+                    MessageBox.Show("Les go baby");
+                }
+
+                
             }
         }
     }
